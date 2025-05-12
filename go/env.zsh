585cc2370eb683b9ef30b1fdf9390466e06916af
Adam Flanagan <adam@avianlabs.net>
2025-05-12 20:18:57 +0100

    Update

---
+++ go/env.zsh
@@ -0,0 +1,3 @@
+export GOROOT="$(brew --prefix go@1.23)/libexec"
+export GOPATH=$HOME
+export PATH=$PATH:$GOPATH/bin
--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,6 +1,3 @@
-# fun: https://bugs.ruby-lang.org/issues/14009
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-
 # init according to man page
 if (( $+commands[rbenv] ))
 then
